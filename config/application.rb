86ec6af015e92043cc28e13014c172bd1138510b
Eduardo Wanderley de Siqueira Araújo <wanderley.eduardo@gmail.com>
2024-07-01 18:04:32 -0300

    adjusting the posts

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,7 @@ require "rails/all"
 Bundler.require(*Rails.groups)
 
 if ['development', 'test'].include? ENV['RAILS_ENV']
-  Dotenv::Railtie.load
+  Dotenv::Rails.load
 end
 
 module EcommerceWander
